b3290e9d3a83f7c6fbd3f271c46e38e447687259
Smetank0105 <206859408+Smetank0105@users.noreply.github.com>
2026-03-17 20:45:08 +0700

    HW DONE

---
+++ Blazor/Components/Pages/Fibonacci.razor.cs
@@ -0,0 +1,20 @@
+﻿using System.Numerics;
+
+namespace Blazor.Components.Pages
+{
+    public partial class Fibonacci
+    {
+        int number = 2;
+        List<BigInteger> result = new List<BigInteger>();
+        void GetFibonacci()
+        {
+            result = new List<BigInteger>();
+            result.Add(0);
+            result.Add(1);
+            for(int i = 2; i < number; i++)
+            {
+                result.Add(result[i-1] + result[i-2]);
+            }
+        }
+    }
+}
---
+++ Blazor/Components/Pages/Power.razor.cs
@@ -0,0 +1,13 @@
+﻿namespace Blazor.Components.Pages
+{
+    public partial class Power
+    {
+        double number = 1;
+        double power = 1;
+        double result = 1;
+        void RaisingToPower()
+        {
+            result = Math.Pow(number, power);
+        }
+    }
+}
