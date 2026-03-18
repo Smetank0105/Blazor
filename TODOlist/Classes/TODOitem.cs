5cef1a3cc9573f2bd681b3aef33e9045f13d80dd
Smetank0105 <206859408+Smetank0105@users.noreply.github.com>
2026-03-18 20:26:11 +0700

    TODOlist DONE

--- TODOlist/Classes/TODOitem.cs
+++ TODOlist/Classes/TODOitem.cs
@@ -2,9 +2,22 @@
 
 namespace TODOlist.Classes
 {
-    public class TODOitem
+    public class TODOitem : IComparable<TODOitem>, IEqualityComparer<TODOitem>
     {
         public string Title {  get; set; }
         public bool isDone { get; set; }
+        public int CompareTo(TODOitem other)
+        {
+            return this.Title.CompareTo(other.Title);
+        }
+        public bool Equals(TODOitem left, TODOitem right)
+        {
+            return left.Title == right.Title;
+        }
+        public int GetHashCode(TODOitem other)
+        {
+            //return other.Title.GetHashCode();
+            return HashCode.Combine(Title);
+        }
     }
 }
