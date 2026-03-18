7a08da8086649ba682ecee7aae1e01d27275ee9a
Smetank0105 <206859408+Smetank0105@users.noreply.github.com>
2026-03-18 20:00:59 +0700

    TODOlist almost DONE

---
+++ TODOlist/Classes/TODOitem.cs
@@ -0,0 +1,10 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace TODOlist.Classes
+{
+    public class TODOitem
+    {
+        public string Title {  get; set; }
+        public bool isDone { get; set; }
+    }
+}
---
+++ TODOlist/Components/Pages/TODO.razor.cs
@@ -0,0 +1,18 @@
+﻿using TODOlist.Classes;
+
+namespace TODOlist.Components.Pages
+{
+    public partial class TODO
+    {
+        List<TODOitem> todos = [];
+        string task;
+        void AddTask()
+        {
+            if (string.IsNullOrWhiteSpace(task)) return;
+            for (int i = 0; i < todos.Count; i++)
+                if (todos[i].Title == task) return;
+            todos.Add(new TODOitem { Title = task});
+            task = "";
+        }
+    }
+}
