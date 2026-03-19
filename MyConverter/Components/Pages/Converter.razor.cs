ac36273c1105e09558746a02326ba3a33937d3b5
Smetank0105 <206859408+Smetank0105@users.noreply.github.com>
2026-03-19 20:36:52 +0700

    Converter done

---
+++ MyConverter/Components/Pages/Converter.razor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace MyConverter.Components.Pages
+{
+    public partial class Converter
+    {
+        string left { get; set; } = "";
+        string right { get; set; } = "";
+        int from_base { get; set; } = 10;
+        int to_base { get; set; } = 10;
+        void ConvertToBaseFromBase()
+        {
+            try
+            {
+                long temp = Convert.ToInt64(left, from_base);
+                right = Convert.ToString(temp, to_base);
+            }
+            catch (Exception ex) { }
+        }
+    }
+}
