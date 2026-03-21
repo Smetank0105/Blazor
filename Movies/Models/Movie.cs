651b86ae43ab3940820c0e72d04f22e6ecd3e7f4
Smetank0105 <206859408+Smetank0105@users.noreply.github.com>
2026-03-21 21:01:12 +0700

    Movie details & URL

---
+++ Movies/Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Movies.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        [Required, StringLength(100, MinimumLength = 3) ]
+        public string Title { get; set; }
+        [Range(typeof(DateOnly), "1888-10-14", "9999-12-31")]
+        public DateOnly ReleaseDate { get; set; }
+        [Required]
+        public string Genre { get; set; }
+        public string? URL { get; set; }
+    }
+}
