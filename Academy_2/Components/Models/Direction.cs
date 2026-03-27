2155c478cb2d395e2f959c9b126b11971f1ac119
Smetank0105 <206859408+Smetank0105@users.noreply.github.com>
2026-03-27 20:26:36 +0700

    Unique Directions FAILED

--- Academy_2/Components/Models/Direction.cs
+++ Academy_2/Components/Models/Direction.cs
@@ -6,6 +6,15 @@ namespace Academy_2.Components.Models
     {
         [Key]
         public byte direction_id { get; set; }
+        [Required]
         public string direction_name { get; set; }
+        //public override bool Equals(object? obj)
+        //{
+        //    return this.direction_name.Equals((obj as Direction).direction_name);
+        //}
+        //public override int GetHashCode()
+        //{
+        //    return HashCode.Combine(direction_name);
+        //}
     }
 }
