da646ed7185623fae0ffcd6dae48be2e502ddbb5
Smetank0105 <206859408+Smetank0105@users.noreply.github.com>
2026-03-27 18:44:02 +0700

    Directions DONE

---
+++ Academy_2/Components/Models/Direction.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Academy_2.Components.Models
+{
+    public class Direction
+    {
+        [Key]
+        public byte direction_id { get; set; }
+        public string direction_name { get; set; }
+    }
+}
---
+++ Academy_2/Data/Academy_2Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Academy_2.Components.Models;
+
+namespace Academy_2.Data
+{
+    public class Academy_2Context : DbContext
+    {
+        public Academy_2Context (DbContextOptions<Academy_2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Academy_2.Components.Models.Direction> Directions { get; set; } = default!;
+    }
+}
